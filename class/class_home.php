7ecf2e062fc0667ed602f698b64690611f6a3883
Hiromichi129 <aska129424@gmail.com>
2023-12-06 23:52:45 +0900

    Initial commit

---
+++ class/class_home.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>診断テスト</title>
+</head>
+
+<body>
+    <div class="main-logo">
+        <a href="../home.php"><img class="main-image" src="../images/header-logo-main.png" ></a>
+    </div>
+    <header>
+        <a href="../home.php">
+            <div class="header-logo"><img class="header-logo" src="../images/header-logo.png"></div>
+        </a>
+        <div class="header-list">
+            <ul>
+                <li>
+                    <a href="../diagnostic_test/test_home.php">
+                        <img class="icon" src="../images/test-icon2.png">
+                        <br>
+                        診断テスト
+                    </a>
+                </li>
+                <li class="diagnostic_test">
+                    <a href="">
+                        <img class="icon" src="../images/class2.png">
+                        <br>
+                        授業を受ける
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img class="icon" src="../images/pencil2.png">
+                        <br>
+                        問題を解く
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img class="icon" src="../images/map2.png">
+                        <br>
+                        学習ロードマップ
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img class="icon" src="../images/stand3.png">
+                        <br>
+                        おすすめの勉強法
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img class="icon" src="../images/books2.png">
+                        <br>
+                        おすすめの参考書・問題集
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img class="icon" src="../images/amature2.png">
+                        <br>
+                        このサイトの使い方
+                    </a>
+                </li>
+                <div class="login">ログイン</div>
+            </ul>
+        </div>
+    </header>
+
+    <div class="main">
+        <div class="contents-wrapper">
+            <div class="container">
+            </div>
+        </div>
+    </div>
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
---
+++ home.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+
+    <title>HOME</title>
+</head>
+
+<body>
+    <div class="main-logo">
+    <a href="home.php"><img class="main-image" src="images/header-logo-main.png"></a>
+    </div>
+    <header>
+        <a href="home.php">
+            <div class="header-logo"><img class="header-logo" src="./images/header-logo.png"></div>
+        </a>
+        <div class="header-list">
+            <ul>
+                <li class="">
+                    <a href="diagnostic_test/test_home.php">
+                        <img class="icon" src="./images/test-icon2.png">
+                        <br>
+                        診断テスト
+                    </a>
+                </li>
+                <li>
+                    <a href="class/class_home.php">
+                        <img class="icon" src="./images/class2.png">
+                        <br>
+                        授業を受ける
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img class="icon" src="./images/pencil2.png">
+                        <br>
+                        問題を解く
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img class="icon" src="./images/map2.png">
+                        <br>
+                        学習ロードマップ
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img class="icon" src="./images/stand3.png">
+                        <br>
+                        おすすめの勉強法
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img class="icon" src="./images/books2.png">
+                        <br>
+                        おすすめの参考書・問題集
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img class="icon" src="./images/amature2.png">
+                        <br>
+                        このサイトの使い方
+                    </a>
+                </li>
+
+                <div class="login">ログイン</div>
+
+            </ul>
+        </div>
+    </header>
+    <div class="main">
+
+        <div class="contents-wrapper">
+            <div class="container">
+                <div class="heading">
+                    <h1>MENU</h1>
+                </div>
+                <div class="items">
+                    <div class="item">
+                        <a href="./diagnostic_test/test_home.php">
+                            <div class="item-icon">
+                                <img src="./images/test-icon2.png">
+                            </div>
+                            <p class="item-title">診断テスト</p>
+                        </a>
+                        <p class="item-text">苦手な単元の苦手な理由を特定します</p>
+                    </div>
+                    <div class="item">
+                        <a href="./class/class_home.php">
+                            <div class="item-icon">
+                                <img src="./images/class2.png">
+                            </div>
+                            <p class="item-title">授業を受ける</p>
+                        </a>
+                        <p class="item-text">好きな単元の授業を受けられます</p>
+                    </div>
+                    <a href="">
+                        <div class="item">
+                            <div class="item-icon">
+                                <img src="./images/pencil2.png">
+                            </div>
+                            <p class="item-title">問題を解く</p>
+                    </a>
+                    <p class="item-text">授業で学習した内容を復習できます</p>
+                </div>
+                <div class="item">
+                    <a href="">
+                        <div class="item-icon">
+                            <img src="./images/map2.png">
+                        </div>
+                        <p class="item-title">学習ロードマップ</p>
+                    </a>
+                    <p class="item-text">学習の進め方を確認できます</p>
+                </div>
+                <div class="item">
+                    <a href="">
+                        <div class="item-icon">
+                            <img src="./images/stand3.png">
+                        </div>
+                        <p class="item-title">おすすめの勉強法</p>
+                    </a>
+                    <p class="item-text">効率的な勉強法を紹介します</p>
+                </div>
+                <div class="item">
+                    <a href="">
+                        <div class="item-icon">
+                            <img src="./images/books2.png">
+                        </div>
+                        <p class="item-title">おすすめの参考書・問題集</p>
+                    </a>
+                    <p class="item-text">おすすめの参考書・問題集を紹介します</p>
+                </div>
+                <div class="item">
+                    <a href="">
+                        <div class="item-icon">
+                            <img src="./images/amature2.png">
+                        </div>
+                        <p class="item-title">このサイトの使い方</p>
+                    </a>
+                    <p class="item-text">当サイトの使い方を確認できます</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    </div>
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
